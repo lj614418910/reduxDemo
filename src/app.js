63c0173829d247ed2c7561c0e7f5e6634357445f
lj614418910 <lijia@riyoshimitekiMacBook-Pro.local>
2016-12-09 10:20:23 +0800

    demo02

--- src/app.js
+++ src/app.js
@@ -1,24 +1,64 @@
+// import {createStore} from 'redux';
 
-function counter (state, action) {
+const counter = (state = 0, action) => {
     switch(action.type) {
         case 'INCREASE':
-            return state + 1;
+            return  state + 1;
         case 'DECREASE':
             return state - 1;
         default:
             return state;
     }
 }
-console.log('current: 0');
-console.log("trgger: INCREASE");
-console.log(counter(0, {
-    type:'INCREASE'
-}));
+
+const createStore = (reducer) => {
+    let state;
+    let list = [];
+    const getState = () => {
+        return state;
+    }
+    const dispatch = (action) =>{
+        state = reducer(state, action);
+        list.forEach((fn) => {
+            fn();
+        })
+    }
+    const subscribe = (fn) => {
+        list.push(fn);
+        return () => {
+            list = list.filter(cd => cd != fn)
+        }
+    }
+    return {
+        getState,
+        subscribe,
+        dispatch
+    }
+}
+
+const store = createStore(counter);
+store.dispatch({
+    type:'INIT',
+})
 
 
 
-console.log('current: 2');
-console.log("trgger: DECREASE");
-console.log(counter(2, {
-    type:'DECREASE'
-}));
+const render = () => {
+    document.getElementsByTagName('body')[0].innerHTML = `<h1> ${store.getState()} </h1>`;
+}
+
+store.subscribe(render);
+
+const unsubsribe = store.subscribe(render);
+
+render();
+
+
+document.addEventListener('click', () => {
+    console.log(1);
+    store.dispatch({
+        type:'INCREASE',
+    })
+    unsubsribe();
+})
+
